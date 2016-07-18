70dcd34a758e6ebe09e216682d4c106e2a79d505
Gculos <garrettculos@gmail.com>
2016-07-17 22:35:34 -0700

    Add Pick Order EV's, update viewCards, add filters

--- app/app.js
+++ app/app.js
@@ -17,7 +17,7 @@ angular.module('myApp', [
  	$routeProvider
 	.when('/', {
 		templateUrl: 'common/views/main.html',
-    	controller: 'AppCtrl'
+    	// controller: 'AppCtrl'
 	})
     .otherwise({
     	templateUrl: 'common/404.html',
@@ -31,22 +31,8 @@ angular.module('myApp', [
         'googleSheetsId':'1QsChUjmQrzyURwWAUtU61EziggAxuG3WO2gbXndRlKU',
         'spectator': false
     }
-    $http.get('AllCards.json').then(function(res){
-        var cards = {'data' : res.data, 'names':[]}; 
-        filterCardObject(cards).then(function(res){
-            $rootScope.cardsNames = res.names;
-            $rootScope.cards = res
-        });
-    });
-
-    function filterCardObject(obj){
-        var deferred = $q.defer();
-        angular.forEach(obj.data ,function(card ,key){
-            obj.names.push(card.name)
-            deferred.resolve(obj)
-        });
-        return deferred.promise;
-    } 
+    $rootScope.cardsNames = [];
+    $rootScope.cards = [];
 })
 
 .config(function ($mdThemingProvider) {
@@ -103,16 +89,16 @@ angular.module('myApp', [
         '100': '#ffffff',
         '200': '#ffffff',
         '300': '#ffffff',
-        '400': '#ffffff',
-        '500': '#ffffff',
-        '600': '#f0f0f0',
-        '700': '#e0e0e0',
-        '800': '#d1d1d1',
-        '900': '#000000',
+        '400': '#f8f8f8',
+        '500': '#ebebeb',
+        '600': '#dedede',
+        '700': '#d1d1d1',
+        '800': '#c5c5c5',
+        '900': '#b8b8b8',
         'A100': '#ffffff',
         'A200': '#ffffff',
         'A400': '#ffffff',
-        'A700': '#e0e0e0',
+        'A700': '#ababab'
         // 'contrastDarkColors': '50 100 200 300 400 500 600 700 800 900 A100 A200 A400 A700'
     };
 
@@ -135,7 +121,7 @@ angular.module('myApp', [
     $scope.draft_player = $rootScope.draft_player;
 
     // --------------------------------------------- //
-    //                  Settings Popup               //
+    //                  Settings Pop-up               //
     // --------------------------------------------- //
 
         // Show Settings Modal (see https://material.angularjs.org/latest/demo/dialog)
@@ -189,7 +175,7 @@ angular.module('myApp', [
         }
 
     // --------------------------------------------- //
-    //                  Pick Popup                   //
+    //                  Pick Pop-up                   //
     // --------------------------------------------- //
 
         // Show Settings Modal (see https://material.angularjs.org/latest/demo/dialog)
@@ -243,18 +229,17 @@ angular.module('myApp', [
                 var sheetsRound = $rootScope.draft_round +1;
                 var sheetLocation = seatToSheet[seat]+sheetsRound;
                 console.log(googleApiKey);
-                var post_url = 'https://sheets.googleapis.com/v4/spreadsheets/'+sheetId+'/values/range?valueInputOption=RAW?key='+googleApiKey;
+                var post_url = 'https://sheets.googleapis.com/v4/spreadsheets/'+sheetId+'/values/Sheet1!C1?valueInputOption=RAW?key='+googleApiKey;
                 var payload = {
-                    range: "Sheet1!"+sheetLocation,
                     majorDimension: "DIMENSION_UNSPECIFIED",
                     values:[$scope.selectedItem]
                 };
                 $http({
-                  method: 'PUT',
-                  authorization: 'Basic '+ googleApiKey,
-                  url: post_url,
-                  contentType: 'application/json',
-                  body:payload
+                    method: 'PUT',
+                    url: post_url,
+                    contentType: 'application/json',
+                    authorization: 'Bearer '+googleApiKey,
+                    body: payload,
                 }).success(function(response) {
                     console.log(response);
                     $mdDialog.hide(answer);
@@ -301,7 +286,10 @@ angular.module('myApp', [
             var deferred = $q.defer();
             if(sheetId){
                 var url = 'https://sheets.googleapis.com/v4/spreadsheets/'+sheetId+'/values/Sheet1!B1:I48?key='+api_key;
-                $http.get(url).success(function(response) {
+                $http({
+                    method: 'GET',
+                    url: url,
+                }).success(function(response) {
                     deferred.resolve(response);
                     return deferred.promise;
                 });
@@ -310,36 +298,69 @@ angular.module('myApp', [
         }
 
         $interval(function(){
-            console.log('Interval Trigger: '+($scope.draft_player+1)+':'+($rootScope.settings.seat_number));
+            // console.log('Interval Trigger: '+($scope.draft_player+1)+':'+($rootScope.settings.seat_number));
 
             if( ($scope.draft_player+1) != $rootScope.settings.seat_number){
-                console.log('Not you');
+                // console.log('Not you');
                 getSheet($rootScope.settings.googleSheetsId).then(function(response){
+                    // set draft
                     $scope.draft = response;
+                    // set players drafting
                     $scope.draft.players = response.values[0];
+                    // set draft array
                     var picks = response.values.splice(1,response.values.length+1);
+                    // determine draft round
                     $rootScope.draft_round = picks.length;
+                    // determine the player who's drafting
                     $scope.draft_player = picks[picks.length-1].length;
+                    // fill in empty picks of draft so it displays correctly
                     formatDraftPicks($scope.draft.players, picks);
+                    // return picked cards list for filtering EPO
+                    returnPickList(picks).then(function(res){
+                        $scope.pickList = res;
+                    });
                 });
             }
+        }, 5000);
 
-        }, 10000);
-
-        $rootScope.$watch('settings', function(){
+        $rootScope.$watch('[settings.googleSheetsId, settings.seat_number]', function(){
             console.log('Watch Sheets Triggered');
+            // change local settings
+            $scope.settings = $rootScope.settings;
+
             getSheet($rootScope.settings.googleSheetsId).then(function(response){
-                // change local settings
-                $scope.settings = $rootScope.settings;
+                // set draft
                 $scope.draft = response;
+                // set players drafting
                 $scope.draft.players = response.values[0];
+                // set draft array
                 var picks = response.values.splice(1,response.values.length+1);
+                // determine draft round
                 $rootScope.draft_round = picks.length;
+                // determine the player who's drafting
                 $scope.draft_player = picks[picks.length-1].length;
+                // fill in empty picks of draft so it displays correctly
                 formatDraftPicks($scope.draft.players, picks);
+                // return picked cards list for filtering EPO
+                returnPickList(picks).then(function(res){
+                    $scope.pickList = res;
+                });
+
 
             });
         },true);
+
+        function returnPickList(pick_array){
+            var arr = [];
+            var deferred = $q.defer();
+            angular.forEach( pick_array, function(pick_round, key1){
+                angular.forEach( pick_round, function(pick, key2){
+                    arr.push(pick.toLowerCase());
+                    deferred.resolve(arr)
+                });    
+            });
+            return deferred.promise;
+        }
 
         function formatDraftPicks(player_array, picks_array){
             var numbPlayers = player_array.length;
@@ -364,10 +385,10 @@ angular.module('myApp', [
 
     // --------------------------------------------- //
     //                 Card Database                 //
-    // --------------------------------------------- //       
-
-        $scope.viewCard = function($event, key1, key2){
-            var cardname = $scope.draft.picks[key1][key2];
+    // --------------------------------------------- //
+        
+        $scope.viewCard = function(cardname){
+            // var cardname = $scope.draft.picks[key1][key2];
             $scope.cardView = $rootScope.cards.data[cardname];
 
             $http.get('https://api.deckbrew.com/mtg/cards?name='+cardname).then(function(res){
@@ -395,30 +416,135 @@ angular.module('myApp', [
     //                 Card Analysis                 //
     // --------------------------------------------- //
 
+        $scope.EPO = [];
 
-        var old_sheets= [   
-            '1QsChUjmQrzyURwWAUtU61EziggAxuG3WO2gbXndRlKU',
-            '1IUKyVd3REbUlfw0d-0cg7m1mDR-tE7vMldzm-CCyxnk',
-            '1zApva1kM-OyAbCFZTpvcJq7rhWNoOsJ88jw8QjjXgoU',
-            '1V1BIspMPOk-AeJjB7xmfqxMu-9W2-Yk1d2ycFbZOyf0',
-            '1j_V7zcnQGdk42rIf3oPXiqTPpF9WfQ6hwgKeGiJwp7c'
-        ];
-        var sheets_data = [];
-
-        angular.forEach(old_sheets, function(sheet_id, key){
-            getSheet(sheet_id).then(function(response){
-                var pick_rows = response.values.length;
-                var pick_order = [];
-                for( var i=1; i<pick_rows; i++){
-                    for( var j=0; j<8; j++){
-                        pick_order.push(response.values[i][j]);
-                    }
-                }
-
-                sheets_data[key] = pick_order;
+        $http.get('AllCards.json').then(function(res){
+            var cards = {'data' : res.data, 'names':[]}; 
+            filterCardObject(cards).then(function(res){
+                // redefine cards and cardsNames
+                $rootScope.cardsNames = res.names;
+                $rootScope.cards = res;
+                
+                //create Expected Pick Order array
+                cardAnalysis($rootScope.cards).then(function(resp){
+                    $rootScope.cards = resp;
+                    angular.forEach(resp.data, function(card, key){
+                        if(card.VRD_EPO !== undefined){
+                            $scope.EPO.push(card);
+                        }
+                    });
+                });
             });
         });
+
+        function filterCardObject(obj){
+            var deferred = $q.defer();
+            angular.forEach(obj.data ,function(card ,key){
+                obj.names.push(card.name)
+                deferred.resolve(obj)
+            });
+            return deferred.promise;
+        }
+
+        function cardAnalysis(cards){
+            var deferred = $q.defer();   
+            var old_sheets= [   
+                '1IUKyVd3REbUlfw0d-0cg7m1mDR-tE7vMldzm-CCyxnk',
+                '1zApva1kM-OyAbCFZTpvcJq7rhWNoOsJ88jw8QjjXgoU',
+                '1V1BIspMPOk-AeJjB7xmfqxMu-9W2-Yk1d2ycFbZOyf0',
+                '1j_V7zcnQGdk42rIf3oPXiqTPpF9WfQ6hwgKeGiJwp7c'
+            ];
+            var sheets_data = [];
+            // console.log($rootScope.cards);
+            angular.forEach(old_sheets, function(sheet_id, key){
+                var sheet_id = old_sheets[key];
+                getSheet(sheet_id).then(function(response){
+                    var pick_rows = response.values.length;
+                    var null_pick_order = [];
+                    var pick_order = [];
+                    for( var i=1; i<pick_rows; i++){
+                        if(i%2 == 0){
+                            for( var j=7; j>=0; j--){
+                                pick_order.push(response.values[i][j]);
+
+                                if(cards.data[response.values[i][j]] !== undefined){
+                                    // Number of time the card has been drafter
+                                    var VRD_numTD = cards.data[response.values[i][j]].VRD_numTD;
+                                    if(VRD_numTD === undefined){
+                                        VRD_numTD = 1;
+                                    } else {
+                                        VRD_numTD++;
+                                    }
+                                    
+                                    // VRD Expected Pick Order
+                                    var VRD_EPO = cards.data[response.values[i][j]].VRD_EPO
+                                    if(VRD_EPO === undefined){
+                                        VRD_EPO = ((i-1)*8)+(8-j);
+                                    } else {
+                                        // if (VRD_EPO < 8){ console.log(VRD_EPO + ' --- ' + ((((i-1)*8)+(8-j)+(VRD_EPO*(VRD_numTD -1)))/VRD_numTD) + ' --- ' + response.values[i][j]); }
+                                        VRD_EPO = ((((i-1)*8)+(8-j)+(VRD_EPO*(VRD_numTD -1)))/VRD_numTD);
+                                    }
+                                    cards.data[response.values[i][j]].VRD_numTD = VRD_numTD;
+                                    cards.data[response.values[i][j]].VRD_EPO = VRD_EPO;
+                                } else {
+                                    // if the card is misspelled 
+                                    null_pick_order.push({
+                                        'name': response.values[i][j],
+                                        'VRD_EP)':(i-1)*8+(8-j), 
+                                        'VRD_numTD':1
+                                    });
+                                }
+
+                                deferred.resolve(cards);
+                            }    
+                        } else {
+                            for( var j=0; j<8; j++){
+                                pick_order.push(response.values[i][j]);
+
+                                if(cards.data[response.values[i][j]] !== undefined){
+                                    // Number of time the card has been drafter
+                                    var VRD_numTD = cards.data[response.values[i][j]].VRD_numTD;
+                                    if(VRD_numTD === undefined){
+                                        VRD_numTD = 1;
+                                    } else {
+                                        VRD_numTD++;
+                                    }
+                                    
+                                    // VRD Expected Pick Order
+                                    var VRD_EPO = cards.data[response.values[i][j]].VRD_EPO
+                                    if(VRD_EPO === undefined){
+                                        VRD_EPO = ((i-1)*8)+j+1;
+                                    } else {
+                                        // if (VRD_EPO < 8){console.log(VRD_EPO + ' --- ' + (((((i-1)*8)+j+1)+(VRD_EPO*(VRD_numTD-1)))/VRD_numTD)+ ' --- ' + response.values[i][j]);}
+                                        VRD_EPO = (((((i-1)*8)+j+1)+(VRD_EPO*(VRD_numTD-1)))/VRD_numTD);
+                                    }
+                                    cards.data[response.values[i][j]].VRD_numTD = VRD_numTD;
+                                    cards.data[response.values[i][j]].VRD_EPO = VRD_EPO;
+                                } else {
+                                    // if the card is misspelled 
+                                    null_pick_order.push({
+                                        'name': response.values[i][j],
+                                        'VRD_EPO':(i-1)*8+(8-j), 
+                                        'VRD_numTD':1
+                                    });
+                                }
+
+                                deferred.resolve(cards);
+                            }
+                        }
+                    }
+                    sheets_data[key] = pick_order;
+                });
+            });
+            
+            return deferred.promise;
+        }
+
+        $scope.removeCard = function(item){
+            $scope.EPO.splice($scope.EPO.indexOf(item),1);
+        }
 }])
+
 .directive('draft', function($window) {
     return function (scope, element) {
         var w = angular.element($window);
@@ -433,6 +559,22 @@ angular.module('myApp', [
         setHeight()
     }
 })
+
+.directive('expected-pick-order',function($window) {
+    return function (scope, element) {
+        var w = angular.element($window);
+        var setHeight = function() {
+            angular.element('draft').css({'height':w.height()});
+        }
+
+        w.trigger('resize', function () {
+            setHeight()
+        });
+
+        setHeight()
+    }
+})
+
 .directive('tbody', function($window) {
     return function(scope, element){
         var w = angular.element($window);
@@ -449,6 +591,7 @@ angular.module('myApp', [
         setHeight()
     }
 })
+
 .directive('cardDragger', ['$document', function($document) {
   return {
     link: function(scope, element, attr) {
@@ -486,4 +629,16 @@ angular.module('myApp', [
       }
     }
   }
-}]);
+}])
+
+.filter('cardChosen', function() {
+    return function(cards, pickList) {
+        var filtered = [];
+        angular.forEach(cards, function(item) {
+            if(pickList.indexOf(item.name.toLowerCase()) < 0) {
+                filtered.push(item);
+            }
+        });
+        return filtered;
+    };
+});
